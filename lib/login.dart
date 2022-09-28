c2401911dc0171a030932b8324d0c4b2c52dc422
Priyam Srivastava <79325116+ipriyam26@users.noreply.github.com>
2022-09-28 15:13:28 +0530

    Scripted auto-commit on change (2022-09-28 15:13:28) by gitwatch.sh

--- lib/login.dart
+++ lib/login.dart
@@ -10,7 +10,7 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           children: [
             const FlutterLogo(),
-            const Container()
+             Container()
 
           ],
         ),
